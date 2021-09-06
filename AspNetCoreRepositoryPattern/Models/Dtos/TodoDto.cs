b79048a01bf76219393f005487714d1cfd38d611
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-09-06 19:55:16 +0200

    updated

--- AspNetCoreRepositoryPattern/Models/Dtos/TodoDto.cs
+++ AspNetCoreRepositoryPattern/Models/Dtos/TodoDto.cs
@@ -3,15 +3,6 @@ namespace AspNetCoreRepositoryPattern.Models.Dtos
 {
     public class TodoDto
     {
-        public TodoDto()
-        {
-        }
-
-        public TodoDto(Guid id)
-        {
-            Id = id;
-        }
-
         public Guid Id { get; set; }
         public string Name { get; set; }
         public bool Done { get; set; }
