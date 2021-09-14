652d1bf1e17eeb44fb7b0e27941b77b0ed39f8d5
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-09-14 07:55:32 +0200

    update

--- AspNetCoreRepositoryPattern/Program.cs
+++ AspNetCoreRepositoryPattern/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Exceptions;
 
 namespace AspNetCoreRepositoryPattern
 {
